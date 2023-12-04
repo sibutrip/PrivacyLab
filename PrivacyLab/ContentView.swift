f18d17229b9bd3e6c11ca475fa2ad1db5bc0d557
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2023-12-04 10:55:25 -0500

    Initial Commit

---
+++ PrivacyLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrivacyLab
+//
+//  Created by Cory Tripathy on 12/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrivacyLab/PrivacyLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivacyLabApp.swift
+//  PrivacyLab
+//
+//  Created by Cory Tripathy on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct PrivacyLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
