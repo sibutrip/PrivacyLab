96a3be63e66b75ef8270c05c7e272d7783aa3766
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2024-01-04 14:08:38 -0500

    changed instruction in location

--- PrivacyLab/Location/MapView.swift
+++ PrivacyLab/Location/MapView.swift
@@ -10,7 +10,7 @@ import MapKit
 
 #warning("Location Step 1: Delay requesting access to the user's location as long as possible. Where do we currently request permission to this resource? Where would be a better place to request that?")
 #warning("Location Step 2: As of now, if you deny permission of the app, the show the user the `mapCameraPosition` which is centered on the United States. Instead of this, let's show the `mapCameraPosition` only if the user has enabled location permissions (use the `hasLocationPermission` property of our `MapViewModel`); otherwise, show a button that takes the user to PrivacyLab's Settings page (hint: use the `navigateToSettings` method in `SettingsService`, look at how we use it `ContactsView` if you get stuck).")
-#warning("Location Step 3: Change the `NSLocationWhenInUseUsageDescription` description in PrivacyLab's info.plist to be more descriptive.")
+#warning("Location Step 3: Change the `Privacy - Location Always and When In Use Usage Description` description in PrivacyLab's info.plist to be more descriptive.")
 
 struct MapView: View {
     @EnvironmentObject var mapViewModel: MapViewModel
